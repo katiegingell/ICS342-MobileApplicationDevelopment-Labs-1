3459d9aece5d54824760d7b155b489328d8c2cbe
Katie Gingell <katiegingell@Katies-MacBook-Pro.local>
2023-06-01 18:15:16 -0500

    Changes commit

--- app/src/main/java/com/ics342/labs/MainActivity.kt
+++ app/src/main/java/com/ics342/labs/MainActivity.kt
@@ -8,13 +8,23 @@ import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var editText: EditText
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button = findViewById(R.id.button)
+        editText = findViewById(R.id.editText)
+
+        button.setOnClickListener{
+            handleButtonClick()
+        }
     }
 
     private fun handleButtonClick() {
         /** Implement the functionality to display the alert here. **/
+        val text = if (editText.text.toString().isEmpty()) showErrorAlert() else showTextInAlert(editText.text.toString())
     }
 
     private fun showTextInAlert(text: String) {
