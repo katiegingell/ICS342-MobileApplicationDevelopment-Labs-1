ef18c8cb765d55da370bf51fd61c25020c13229e
Katie Gingell <katiegingell@Katies-MacBook-Pro.local>
2023-07-10 21:07:08 -0500

    Lab 6

--- app/src/main/java/com/ics342/labs/Data.kt
+++ app/src/main/java/com/ics342/labs/Data.kt
@@ -1,3 +1,11 @@
 package com.ics342.labs
 
+import com.squareup.moshi.Json
+
 // Add the data classes here.
+data class Data (
+    @Json(name = "id") val id : Int,
+    @Json(name = "give_name") val giveName: String,
+    @Json(name = "family_name") val familyName: String,
+    @Json(name = "age") val age: Int,
+        )
--- app/src/main/java/com/ics342/labs/MainActivity.kt
+++ app/src/main/java/com/ics342/labs/MainActivity.kt
@@ -4,16 +4,23 @@ import android.content.res.Resources
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ics342.labs.ui.theme.LabsTheme
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.adapter
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,9 +34,25 @@ class MainActivity : ComponentActivity() {
                     /*
                     Display the items from the Json file in a LazyColumn
                      */
+                    LazyColumn{
+                        items(items = data){
+                            personalDataView(data = it)
+                        }
+                    }
                 }
             }
         }
+    }
+}
+
+@Composable
+private fun personalDataView(data: Data){
+    Column(modifier = Modifier
+        .padding(5.dp)) {
+        Text(text = "ID: " + data.id.toString())
+        Text(text = "First Name: " + data.giveName)
+        Text(text = "Last Name: " + data.familyName)
+        Text(text = "Age: " + data.age.toString())
     }
 }
 
@@ -39,9 +62,9 @@ private fun loadData(resources: Resources): String {
         .bufferedReader()
         .use { it.readText() }
 }
-
-private fun dataFromJsonString(json: String): List</* Add your data type here */> {
-    val moshi: Moshi = Moshi.Builder().build()
-    val jsonAdapter: JsonAdapter<List</* Put your data type here */>> = moshi.adapter<List</* put your data type here*/>>()
-    return jsonAdapter.fromJson(json)
+@OptIn(ExperimentalStdlibApi::class)
+private fun dataFromJsonString(json: String): List<Data> {
+    val moshi: Moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
+    val jsonAdapter: JsonAdapter<List<Data>> = moshi.adapter()
+    return jsonAdapter.fromJson(json) ?: listOf()
 }
